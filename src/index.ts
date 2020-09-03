559b5135647b0e109cab3b0323b6b5c5e0b87810
isbhargav <ladbhargav@yahoo.co.in>
2020-09-02 22:13:44 -0400

    Added Mongoose schema

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,16 @@
 import express from "express";
-// import { ApolloServer } from "apollo-server-express";
-import mongoose from "mongoose";
-import User from "./models/User";
+import { connectToMongoDB } from "./utils/db";
+import { config } from "dotenv";
+config();
 
 const main = async () => {
-  const app = express();
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/insta", {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    });
-    console.log(`Connected to database ${conn.connections[0].name}`);
+    const app = express();
+    connectToMongoDB(process.env.MONGO_URI);
+
+    app.listen(5000, () => console.log("Express Server is up"));
   } catch (err) {
     console.error(err);
   }
-
-  app.listen(4000, () => console.log("Express Server is up"));
 };
 main();
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
-export const connectToMongoDB = async () => {
+export const connectToMongoDB = async (uri: string) => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017", {
+    const conn = await mongoose.connect(uri, {
       useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
     });
-    console.log(`Connected to database ${connection.conn[0].name}`);
+    console.log(`Connected to database ${conn.connections[0].name}`);
   } catch (err) {
     console.error(err);
   }
